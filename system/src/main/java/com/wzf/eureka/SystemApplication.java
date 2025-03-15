67b183f26f6fa130dfa44983c1b0770a4c14d04d
王智芳 <80504767@qq.com>
2025-03-15 15:35:01 +0800

    2.初始化system模块

--- system/src/main/java/com/wzf/eureka/SystemApplication.java
+++ system/src/main/java/com/wzf/eureka/SystemApplication.java
@@ -5,20 +5,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplication {
+@EnableDiscoveryClient
+public class SystemApplication {
 
-	private static  final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+	private static  final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		SpringApplication app = new SpringApplication(SystemApplication.class);
 		ConfigurableEnvironment environment = app.run(args).getEnvironment();
 		LOG.info("启动成功！");
-		LOG.info("Eureka地址: \thttp://127.0.0.1:{}",environment.getProperty("server.port"));
+		LOG.info("System地址: \thttp://127.0.0.1:{}",environment.getProperty("server.port"));
 	}
 
 }
