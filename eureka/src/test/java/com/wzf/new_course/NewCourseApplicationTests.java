369503706431859788d81bf05b7b174959eb8c5c
王智芳 <80504767@qq.com>
2025-03-15 15:06:52 +0800

    1.初始化注册中心

---
+++ eureka/src/test/java/com/wzf/new_course/NewCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzf.new_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
